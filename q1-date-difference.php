368cb3770b9984a77941d7a4a8cb996a7a9ab037
Paul Ndalila <paulndalila001@gmail.com>
2023-12-10 15:08:36 +0300

    init
    Date difference

---
+++ q1-date-difference.php
@@ -0,0 +1,30 @@
+<?php
+
+    class CalculateDifference {
+        private $startDate;
+        private $endDate;
+
+        public function __construct($startDate, $endDate) {
+            $this->startDate = new DateTime($startDate);
+            $this->endDate = new DateTime($endDate);
+        }
+
+        public function getDateDifference() {
+            $theDifference = $this->startDate->diff($this->endDate);
+            return $theDifference;
+        }
+    }
+
+    
+    $startDate = '1981-11-03';
+    $endDate = '2013-09-04';
+
+    //instatiate class
+    $dateDifferenceCalculator = new CalculateDifference('1981-11-03', '2013-09-04');
+
+    // Getting the difference and display
+    $date_difference = $dateDifferenceCalculator->getDateDifference();
+    echo "The date difference between <b>".$startDate."</b> and <b>".$endDate."</b> is: <b>".$date_difference->y." years, ".$date_difference->m." months, ".$date_difference->d." days</b>";
+
+
+?>
