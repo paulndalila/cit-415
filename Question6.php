6fb41ca33f2a5e86cc5843d5e75ee434e56714cb
Paul Ndalila <paulndalila001@gmail.com>
2024-02-19 14:04:51 +0300

    compressor

--- Question6.php
+++ Question6.php
@@ -1,1 +1,28 @@
-
+<?php
+    class Compressor {
+        function compress($chararacters) {
+            $compressed = '';
+            $count = 1;
+            $length = count($chararacters);
+    
+            for ($i = 0; $i < $length; $i++) {
+                if ($i < $length - 1 && $chararacters[$i] === $chararacters[$i + 1]) {
+                    $count++;
+                } else {
+                    if ($count > 1) {
+                        $compressed .= $chararacters[$i - 1] . $count;
+                    } else {
+                        $compressed .= $chararacters[$i];
+                    }
+                    $count = 1;
+                }
+            }
+            return $compressed;
+        }
+    }
+    
+    //instatiate class
+    $compress = new Compressor();    
+    $chars = ["a", "a", "b", "b", "c", "c", "c"];
+    echo $compress->compress($chars);
+?>
